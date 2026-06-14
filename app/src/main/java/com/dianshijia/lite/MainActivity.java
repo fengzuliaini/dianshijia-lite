1dae6a22b6043e6617b0c4002bea5de229f5fd5e
刘春奇 <lcq@liuchunqideMac-mini.local>
2026-06-14 08:36:53 +0800

    fix: 导入缺失的 android.content.Intent 类

--- app/src/main/java/com/dianshijia/lite/MainActivity.java
+++ app/src/main/java/com/dianshijia/lite/MainActivity.java
@@ -1,6 +1,7 @@
 package com.dianshijia.lite;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
